a46bf19abf3916d48bd575a7467b584fefa8193f
lcondon <lcondon@bennington.edu>
2018-08-15 15:31:30 -0400

    Delete play.js

--- play.js
+++
@@ -1,8 +0,0 @@
-var _ = require('lodash')
-
-var string = 'i am a route NAME';
-console.log(string);
-string = _.camelCase(string)
-console.log(string);
-string = _.camelCase(_.toLower('bbq BURGER with avocADO on THE Side'));
-console.log(string);
