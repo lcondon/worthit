c70fb3d75e8d0ecce9628008a725c528484762ac
Kate Gibbs <kategibbs@Kates-MacBook-Pro-2.local>
2018-08-22 20:59:56 -0400

    results handlebars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -39,46 +39,54 @@ htmlRouter.get('/movies', function (req, res) {
   }
 });
 
-htmlRouter.get("/login", function (req, res) {
-  res.sendFile(path.join(__dirname + "/../index.html"));
-});
+htmlRouter.get('/movies/:category', function (req, res) {
+  db.Movie.findAll({ where: { genres: { $like: "%" + req.params.category + "%" } } }).then(function (movies) {
+    // res.render('results', movies: movies);
 
-htmlRouter.post('/login',
-  passport.authenticate('local', {
-    successRedirect: '/favorites',
-    failureRedirect: '/login',
-    failureFlash: true
-  })
-);
-
-htmlRouter.get("/favorites", function (req, res) {
-  // db.Example.findAll({}).then(function (dbExamples) {
-  //   res.render("index", {
-  //     msg: "Welcome!",
-  //     examples: dbExamples
-  //   });
-  // });
-  res.send(":)");
-});
-
-htmlRouter.get('/auth', function (req, res) {
-  console.log(`User authenticated? ${req.isAuthenticated()}`);
-  if (req.isAuthenticated()) {
-    res.send('you hit the authentication endpoint\n')
-  } else {
-    res.redirect('/')
-  }
+    res.json(movies)
+  });
 })
 
-htmlRouter.get('/logout', function (req, res) {
-  req.logout();
-  res.redirect('/');
-});
+  htmlRouter.get("/login", function (req, res) {
+    res.sendFile(path.join(__dirname + "/../index.html"));
+  });
 
-// Render 404 page for any unmatched routes
-// htmlRouter.get("*", function (req, res) {
-//   res.render("404");
-// });
+  htmlRouter.post('/login',
+    passport.authenticate('local', {
+      successRedirect: '/favorites',
+      failureRedirect: '/login',
+      failureFlash: true
+    })
+  );
 
-module.exports = htmlRouter;
+  htmlRouter.get("/favorites", function (req, res) {
+    // db.Example.findAll({}).then(function (dbExamples) {
+    //   res.render("index", {
+    //     msg: "Welcome!",
+    //     examples: dbExamples
+    //   });
+    // });
+    res.send(":)");
+  });
+
+  htmlRouter.get('/auth', function (req, res) {
+    console.log(`User authenticated? ${req.isAuthenticated()}`);
+    if (req.isAuthenticated()) {
+      res.send('you hit the authentication endpoint\n')
+    } else {
+      res.redirect('/')
+    }
+  })
+
+  htmlRouter.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
+  });
+
+  // Render 404 page for any unmatched routes
+  // htmlRouter.get("*", function (req, res) {
+  //   res.render("404");
+  // });
+
+  module.exports = htmlRouter;
 
