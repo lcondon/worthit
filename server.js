b8dae439da2adcce14d5a193893bfe83035a57c2
Lucas Condon <lcondon@bennington.edu>
2018-08-27 14:44:30 -0400

    force sync

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.engine("handlebars", bars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://radiant-anchorage-14785.herokuapp.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
